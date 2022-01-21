089076a7e72ff2456854cb19993e15e8ea05ed7d
alias <alias@DESKTOP-S9FH6DL>
2022-01-21 13:37:50 -0500

    Add project files.

---
+++ Test/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,28 @@
+﻿using ACM.bl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Aliasgar",
+                LastName = "Gandhi"
+            };
+            string actual = "Gandhi, Aliasgar";
+
+            //Act
+            string expected = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Test/ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d7b4f552-aa35-4234-8b6a-4b6dfdd20a47")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
